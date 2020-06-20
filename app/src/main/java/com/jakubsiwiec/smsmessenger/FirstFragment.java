a42dbb1edbafaadd4a534ddef8be965dcc14bc35
Jakub Siwiec <jakubsiwiec.nwm@gmail.com>
2020-06-20 17:30:47 +0200

    Create layout for custom dialog and calling method

--- app/src/main/java/com/jakubsiwiec/smsmessenger/FirstFragment.java
+++ app/src/main/java/com/jakubsiwiec/smsmessenger/FirstFragment.java
@@ -1,9 +1,15 @@
 package com.jakubsiwiec.smsmessenger;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.database.Cursor;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -12,6 +18,44 @@ import androidx.navigation.fragment.NavHostFragment;
 public class FirstFragment extends Fragment {
 
     private DataBaseHelper dataBaseHelper;
+
+    //IF LIST VIEW IS EMPTY,
+    //CREATE SOME FANCY NOTE THAT INFORMS USER THAT
+    //THEY HAVE NO CHATS YET
+
+
+    private AlertDialog addContactDialog(){
+
+        // Build an AlertDialog
+        AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+
+        LayoutInflater inflater = getLayoutInflater();
+        View dialogView = inflater.inflate(R.layout.alertdialog_custom_view,null);
+
+        // Specify alert dialog is not cancelable/not ignorable
+        builder.setCancelable(false);
+
+        // Set the custom layout as alert dialog view
+        builder.setView(dialogView);
+
+        // Get the custom alert dialog view widgets reference
+        final EditText editTextNameAddContact = (EditText) dialogView.findViewById(R.id.editTextNameAddContact);
+        final EditText editTextPhoneAddContact = (EditText) dialogView.findViewById(R.id.editTextPhoneAddContact);
+
+        builder.setPositiveButton("ADD", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                // User clicked OK button
+            }
+        });
+        builder.setNegativeButton("CANCEL", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                //
+            }
+        });
+
+        // Create the alert dialog
+        return builder.create();
+    }
 
     @Override
     public View onCreateView(
@@ -30,6 +74,16 @@ public class FirstFragment extends Fragment {
             public void onClick(View view) {
                 NavHostFragment.findNavController(FirstFragment.this)
                         .navigate(R.id.action_FirstFragment_to_SecondFragment);
+            }
+        });
+
+        view.findViewById(R.id.button_add_contact).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog deleteDialog = addContactDialog();
+                deleteDialog.show();
+
+
             }
         });
     }
