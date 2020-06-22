6bf0844663372e3a72077aa64b1e97a8797b758c
Jakub Siwiec <jakubsiwiec.nwm@gmail.com>
2020-06-22 13:37:06 +0200

    Receive an sms and toast it's text

--- app/src/main/java/com/jakubsiwiec/smsmessenger/MainActivity.java
+++ app/src/main/java/com/jakubsiwiec/smsmessenger/MainActivity.java
@@ -1,11 +1,14 @@
 package com.jakubsiwiec.smsmessenger;
 
+import android.Manifest;
 import android.app.Activity;
 import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -13,10 +16,14 @@ import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.FragmentManager;
 import androidx.navigation.fragment.NavHostFragment;
 
+import android.service.media.MediaBrowserService;
 import android.telephony.SmsManager;
+import android.telephony.SmsMessage;
 import android.util.Log;
 import android.view.View;
 
@@ -28,8 +35,11 @@ public class MainActivity extends AppCompatActivity {
 
     private String SENT = "SMS SENT";
     private String DELIVERED = "SMS DELIVERED";
-    public  PendingIntent sentPI, deliveredPI;
-    private BroadcastReceiver smsSentReceiver, smsDeliveredReceiver;
+    private String RECEIVED = "SMS RECEIVED";
+    public  PendingIntent sentPI, deliveredPI, receivedPI;
+    private BroadcastReceiver smsSentReceiver;
+
+    private static final int MY_PERMISSION_REQUEST_RECEIVE_SMS = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,6 +55,17 @@ public class MainActivity extends AppCompatActivity {
 
         sentPI = PendingIntent.getBroadcast(this, 0, new Intent(SENT), 0);
         deliveredPI = PendingIntent.getBroadcast(this, 0, new Intent(DELIVERED), 0);
+        receivedPI = PendingIntent.getBroadcast(this, 0, new Intent(RECEIVED), 0);
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.RECEIVE_SMS) != PackageManager.PERMISSION_GRANTED){
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.RECEIVE_SMS)){
+                // User denied, do nothing
+            }
+            else{
+                // Request permission
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.RECEIVE_SMS}, MY_PERMISSION_REQUEST_RECEIVE_SMS);
+            }
+        }
 
     }
 
@@ -101,26 +122,9 @@ public class MainActivity extends AppCompatActivity {
             }
         };
 
-        smsDeliveredReceiver = new BroadcastReceiver() {
-            @Override
-            public void onReceive(Context context, Intent intent) {
 
-                switch (getResultCode()){
-
-                    case Activity.RESULT_OK:
-                        Toast.makeText(MainActivity.this, "SMS delivered", Toast.LENGTH_SHORT).show();
-                        break;
-                    case Activity.RESULT_CANCELED:
-                        Toast.makeText(MainActivity.this, "SMS not delivered :(", Toast.LENGTH_SHORT).show();
-                        break;
-
-                }
-
-            }
-        };
 
         registerReceiver(smsSentReceiver, new IntentFilter(SENT));
-        registerReceiver(smsDeliveredReceiver, new IntentFilter(DELIVERED));
 
     }
 
@@ -129,6 +133,5 @@ public class MainActivity extends AppCompatActivity {
         super.onPause();
 
         unregisterReceiver(smsSentReceiver);
-        unregisterReceiver(smsDeliveredReceiver);
     }
 }
